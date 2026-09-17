1f2abebcdd0c5d6689aacf8adbf77f07fa7cdc1e
8Camia-Chloe Faith <clcolumbino@carc.pshs.edu.ph>
2026-09-17 16:09:29 +0800

    Add files via upload

---
+++ 5. Student Score Entry.py
@@ -0,0 +1,10 @@
+# Chloe Faith Columbino
+# 8-Camia
+# Student Score Entry
+
+score = int(input("Please examination score: "))
+
+if 0 <= score <= 100:
+    print("Valid score.")
+else:
+    print("Invalid input. Please enter a number")
