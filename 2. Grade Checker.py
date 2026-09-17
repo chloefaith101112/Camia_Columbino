70a656b2c8ceca2d1408d6126e570dff872836d5
8Camia-Chloe Faith <clcolumbino@carc.pshs.edu.ph>
2026-09-17 15:51:16 +0800

    Add files via upload

---
+++ 2. Grade Checker.py
@@ -0,0 +1,10 @@
+# Chloe Faith Columbino
+# 8-Camia
+# Grade Checker
+
+grade = int(input("Please enter your grade: "))
+
+if 0 <= grade <= 100:
+    print("Valid grade")
+else:
+    print("Invalid grade. Grade must be between 0 and 100.")
